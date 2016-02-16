065aab711dfc97852771f094fa343aea7666e30b
not-an-aardvark <not-an-aardvark@users.noreply.github.com>
2016-02-15 20:53:32 -0500

    Improved error handling for browser parser

--- webparser_src.js
+++ webparser_src.js
@@ -1,6 +1,13 @@
 'use strict';
+function setOutput (obj) {
+  document.getElementById('output').innerHTML = obj instanceof Error ? obj : JSON.stringify(obj, null, 4);
+}
 window.parseFile = fileList => {
-  require('blob-to-buffer')(fileList[0].slice(0), (err, result) => {
-    document.getElementById('output').innerHTML = JSON.stringify(err || require('.').parseBuffer(result), null, 4);
+  require('blob-to-buffer')(fileList[0].slice(0), (blobErr, result) => {
+    try {
+      setOutput(blobErr || require('.').parseBuffer(result), null, 4);
+    } catch (parseErr) {
+      setOutput(parseErr);
+    }
   });
 };
